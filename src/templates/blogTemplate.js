42dabef328e2eb7bc82dc106a87b089dedb86e7b
Kanishk Singh <kanishk.singh198@gmail.com>
2022-11-08 17:22:13 +0530

    added html rendering from blogpost md

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,8 +1,32 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { graphql } from "gatsby"
 
-const Blog = () => {
-  return <Layout>This is the blog template</Layout>
+export const query = graphql`
+  query ($slug: String) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`
+// $slug is the query variable coming from the
+// context slug set up in createPage in gatsby-node.js
+
+const Blog = props => {
+  // props contains the query exported above
+  return (
+    <Layout>
+      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+      <p>{props.data.markdownRemark.frontmatter.date}</p>
+      <div
+        dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
+      ></div>
+    </Layout>
+  )
 }
 
 export default Blog
