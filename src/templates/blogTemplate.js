f149cc2b5581d59814604219d75d3025efbab0e2
Kanishk Singh <kanishk.singh198@gmail.com>
2022-11-12 00:20:03 +0530

    added rich text rendering from Contentful

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,30 +1,47 @@
 import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
+import { renderRichText } from "gatsby-source-contentful/rich-text"
 
-export const query = graphql`
-  query ($slug: String) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      frontmatter {
-        title
-        date
+// Following code is for querying blogposts coming from Contentful.
+export const blogData = graphql`
+  query ($slug: String!) {
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      date(formatString: "MMM Do, YYYY")
+      body {
+        raw
+        references {
+          ... on ContentfulAsset {
+            contentful_id
+            __typename
+            url
+          }
+        }
       }
-      html
     }
   }
 `
-// $slug is the query variable coming from the
-// context slug set up in createPage in gatsby-node.js
 
-const Blog = props => {
-  // props contains the query exported above
+const Blog = ({ data }) => {
+  // options is defined for rendering images in rich-text body from Contentful.
+  const options = {
+    renderNode: {
+      "embedded-asset-block": node => {
+        return (
+          <>
+            <img src={node.data.target.url} alt={node.data.target.title} />
+          </>
+        )
+      },
+    },
+  }
+
   return (
     <Layout>
-      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-      <p>{props.data.markdownRemark.frontmatter.date}</p>
-      <div
-        dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
-      ></div>
+      <h1>{data.contentfulBlogPost.title}</h1>
+      <p>{data.contentfulBlogPost.date}</p>
+      <p>{renderRichText(data.contentfulBlogPost.body, options)}</p>
     </Layout>
   )
 }
