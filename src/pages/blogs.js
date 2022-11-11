f166170120ea695a5b50396aeaf1cb6de259bdcc
Kanishk Singh <kanishk.singh198@gmail.com>
2022-11-12 00:21:32 +0530

    Minor fixes

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -1,30 +1,9 @@
 import React from "react"
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import { graphql, Link, useStaticQuery } from "gatsby"
 import * as blogStyles from "./blogs.module.scss"
 
 const BlogPage = () => {
-  //query for posts in md files
-  // const blogData = useStaticQuery(graphql`
-  //   query {
-  //     allMarkdownRemark {
-  //       edges {
-  //         node {
-  //           frontmatter {
-  //             title
-  //             date
-  //           }
-  //           html
-  //           excerpt
-  //           fields {
-  //             slug
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // `)
-
   // query for posts from Contentful
   const blogData = useStaticQuery(graphql`
     query {
@@ -48,7 +27,7 @@ const BlogPage = () => {
         {blogData.allContentfulBlogPost.edges.map(post => {
           return (
             <li className={blogStyles.post}>
-              <Link to={`/blog/${post.node.slug}`} className="blogLink">
+              <Link to={`/blogs/${post.node.slug}`} className="blogLink">
                 <h2>{post.node.title}</h2>
                 <p className={blogStyles.date}>{post.node.date}</p>
                 {/* <p>{post.node.excerpt}</p> */}
